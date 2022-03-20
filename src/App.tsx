4ea49ad24d6602299d1dcbda0f72634923942f28
Ivan Nechaev <dreamsoulfly@hotmail.com>
2022-03-20 23:10:43 +0300

    static build

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,25 @@
 import React from 'react';
+import {Column} from "./content/Column";
+import {Card} from './content/Card';
+import {AppContainer} from "./styles";
 
 function App() {
   return (
     <div className="App">
+      <AppContainer>
+
+        <Column text="To Do">
+          <Card text="Generate App scaffold"/>
+        </Column>
+
+        <Column text="In Progress">
+          <Card text="Learn Typescript"/>
+        </Column>
+
+        <Column text="Done">
+          <Card text="Begin to use static typing"/>
+        </Column>
+      </AppContainer>
 
     </div>
   );
---
+++ src/content/Card.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {CardContainer} from "../styles";
+
+type CardProps = {
+  text: string
+}
+
+export const Card = ({text}: CardProps) => {
+  return (
+    <CardContainer>{text}</CardContainer>
+  );
+};
